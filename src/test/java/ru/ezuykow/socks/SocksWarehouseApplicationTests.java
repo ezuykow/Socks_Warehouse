604aecf09776b98db2165e23086d5b95f3924ffb
ezuykow <zuy1996@gmail.com>
2023-08-03 18:33:51 +0300

    Initial commit

---
+++ src/test/java/ru/ezuykow/socks/SocksWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.socks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
